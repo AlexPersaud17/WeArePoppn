0914b6c4211f5b8c7b6987a15641da85977e540a
Lou Gaeta <louis.gaeta1@gmail.com>
2017-06-09 12:25:24 -0400

    begins skeleton for user dashboard

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,11 +4,15 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def show
+    @user = current_user if logged_in?
+  end
+
   def create
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      redirect_to root_path
+      redirect_to @user
     else
       @errors = @user.errors.full_messages
       render :new
