835f316ff192cbf100ef29d9ee6aa80771246ddb
Lou Gaeta <louis.gaeta1@gmail.com>
2017-06-09 11:52:39 -0400

    Remove unnecessary instance variable

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def current_user
-    @current_user ||= User.find_by_id(session[:user_id])
+    User.find_by_id(session[:user_id])
   end
   def logged_in?
     !!current_user
