0982ca2d56e020eba9da944f54c8fd2583999b19
Alex Persaud <alex_persaud01@yahoo.com>
2017-06-09 11:22:24 -0400

    Incorporate bootstrap into our forms:

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ApplicationRecord
 
-  validates :first_name, :last_name, :email, :password, presence: true
+  validates :first_name, :last_name, :email, presence: true
   validates :email, uniqueness: true
   has_secure_password
 
   def full_name
     first_name + " " + last_name
   end
-  
+
 end
