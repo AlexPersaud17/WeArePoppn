19a5bb51f06334f4ad8399b78f004030590e3593
Lou Gaeta <louis.gaeta1@gmail.com>
2017-06-10 12:21:35 -0400

    Destroy dependencies when destroying party

--- app/models/party.rb
+++ app/models/party.rb
@@ -1,8 +1,8 @@
 class Party < ApplicationRecord
   belongs_to :host, class_name: "User"
-  has_many :party_items
+  has_many :party_items, dependent: :destroy
   has_many :items, through: :party_items
-  has_many :guests
+  has_many :guests, dependent: :destroy
   has_many :comments, through: :guests
 
   validates :name, :description, :location, :date, presence: true
