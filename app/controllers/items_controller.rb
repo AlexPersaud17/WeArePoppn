95ea88ac258a055ea56ddace8e0d5e62d331de62
theasteve <steven.aguilar@stu.bmcc.cuny.edu>
2017-06-10 13:03:40 -0400

    request information from api to store it in @recipe variable

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,6 +3,23 @@ class ItemsController < ApplicationController
     @item = Item.new
   end
 
+  def show
+    item = Item.find(params[:id])
+    if item.category == "food"
+      query = item.name.gsub(' ', '%20')
+      search_uri = URI.parse("http://food2fork.com/api/search?key=88f3ebacdda487a8f189e14742f852cd&q=#{query}")
+      search_response = Net::HTTP.get_response(search_uri)
+      search_results = JSON.parse(search_response.body)
+      recipe = search_results["recipes"][0]
+
+      recipe_uri = URI.parse("http://food2fork.com/api/get?key=88f3ebacdda487a8f189e14742f852cd&rId=#{recipe["recipe_id"]}")
+      recipe_res = Net::HTTP.get_response(recipe_uri)
+      recipe_data = JSON.parse(recipe_res.body)
+      @recipe = recipe_data["recipe"]
+    end
+
+  end
+
   def create
   end
 
