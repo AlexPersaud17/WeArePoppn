cfaa1dc7d95505ff260b548dd1a5338c0d3c588f
Lou Gaeta <louis.gaeta1@gmail.com>
2017-06-10 13:20:11 -0400

    Complete creation of list_item

--- app/controllers/list_items_controller.rb
+++ app/controllers/list_items_controller.rb
@@ -4,7 +4,9 @@ class ListItemsController < ApplicationController
     @party = Party.find(params[:party_id])
     @item = Item.find(params[:item_id])
     @party_item = PartyItem.find_by(party_id: @party.id, item_id: @item.id)
-    binding.pry
+    @guest = Guest.find_by(user: current_user, party: @party)
+    list_item = ListItem.create(guest: @guest, party_item: @party_item)
+    redirect_to @party
   end
 
 end
