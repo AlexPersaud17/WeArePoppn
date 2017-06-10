41075838938f81a384c9e505194f3eaf6c96dd50
theasteve <steven.aguilar@stu.bmcc.cuny.edu>
2017-06-10 16:44:10 -0400

    refactor items controller to remove api key and insert an enviroment (ENV) variable

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,15 +8,16 @@ class ItemsController < ApplicationController
     item = Item.find(params[:id])
     if item.category == "food"
       query = item.name.gsub(' ', '%20')
-      search_uri = URI.parse("http://food2fork.com/api/search?key=88f3ebacdda487a8f189e14742f852cd&q=#{query}")
+      search_uri = URI.parse("http://food2fork.com/api/search?key=#{ENV['FOOD_TO_FORK_KEY']}&q=#{query}")
       search_response = Net::HTTP.get_response(search_uri)
       search_results = JSON.parse(search_response.body)
       recipe = search_results["recipes"][0]
 
-      recipe_uri = URI.parse("http://food2fork.com/api/get?key=88f3ebacdda487a8f189e14742f852cd&rId=#{recipe["recipe_id"]}")
+      recipe_uri = URI.parse("http://food2fork.com/api/get?key=#{ENV['FOOD_TO_FORK_KEY']}&rId=#{recipe["recipe_id"]}")
       recipe_res = Net::HTTP.get_response(recipe_uri)
       recipe_data = JSON.parse(recipe_res.body)
       @recipe = recipe_data["recipe"]
+
     end
 
   end
