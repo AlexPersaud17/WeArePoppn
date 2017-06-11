5ec9072d284af5d68024d547201d1a986977fe41
Lou Gaeta <louis.gaeta1@gmail.com>
2017-06-11 13:14:35 -0400

    Add migration to change party date to type datetime

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170609135944) do
+ActiveRecord::Schema.define(version: 20170611170146) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,7 +52,7 @@ ActiveRecord::Schema.define(version: 20170609135944) do
     t.string   "name",        null: false
     t.text     "description", null: false
     t.string   "location",    null: false
-    t.date     "date",        null: false
+    t.datetime "date",        null: false
     t.integer  "host_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
