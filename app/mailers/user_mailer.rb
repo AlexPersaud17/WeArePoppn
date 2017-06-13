d056a18f5d14014412ef6d23d2d59b30dfe26c05
Alex Persaud <alex_persaud01@yahoo.com>
2017-06-13 16:47:30 -0400

    Config production

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,13 +3,13 @@ class UserMailer < ApplicationMailer
 
   def welcome_email(user)
     @user = user
-    @url = 'http://localhost:3000/sessions/new'
+    @url = 'http://partyplanning.herokuapp.comsessions/new'
     mail(to: @user.email, subject: "Welcome to Popp'n")
   end
 
   def invited_email(user, temp)
     @user = user
-    @url = 'http://localhost:3000/users/new'
+    @url = 'http://partyplanning.herokuapp.com/users/new'
     @password = temp
     mail(to: @user.email, subject: "You have been invited to Popp'n")
   end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,4 +85,16 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   config.serve_static_assets = true
   config.assets.compile = true
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'gmail.com',
+    user_name:            ENV["GMAIL_ADDRESS"],
+    password:             ENV["GMAIL_PASSWORD"],
+    authentication:       'plain',
+    enable_starttls_auto: true
+   }
+
 end
