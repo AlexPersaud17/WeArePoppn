155ffeebaa016dbf6c71c98dcdbe1976886471f1
theasteve <steven.aguilar@stu.bmcc.cuny.edu>
2017-06-13 11:58:24 -0400

    create invited email action to send to user when is not signed up

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,15 @@
 class UserMailer < ApplicationMailer
-  default from: 'notifications@example.com'
+  default from: 'theasteve17@gmail.com'
 
   def welcome_email(user)
     @user = user
     @url = 'http://localhost:3000/login'
     mail(to: @user.email, subject: "Welcome to Popp'n")
+  end
+
+  def invited_email(user)
+    @user = user
+    @url = 'http://localhost:3000/login'
+    mail(to: @user.email, subject: "You have been invited to Popp'n")
   end
 end
