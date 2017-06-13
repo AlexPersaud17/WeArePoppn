1a11da5b4d0a3cb99290981372738ddd6d408d61
Lou Gaeta <louis.gaeta1@gmail.com>
2017-06-13 14:53:19 -0400

    Further refactor to use only guest

--- app/models/party_item.rb
+++ app/models/party_item.rb
@@ -2,7 +2,7 @@ class PartyItem < ApplicationRecord
   belongs_to :item
   belongs_to :party
   has_one :list_item, dependent: :destroy
-  has_many :guests, through: :list_items
+  has_one :guest, through: :list_item
 
 
 end
