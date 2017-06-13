d4b361cded9358d37127d7f878b015177e866211
theasteve <steven.aguilar@stu.bmcc.cuny.edu>
2017-06-13 15:17:18 -0400

    set action mailer default url to localhost:3000

--- config/application.rb
+++ config/application.rb
@@ -21,5 +21,7 @@ module FinalProject
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.action_mailer.default_url_options = { host: 'localhost:3000' }
   end
 end
