1d991ecf6e9f5b8516d4f255e8d0d6e6f37889cd
Lou Gaeta <louis.gaeta1@gmail.com>
2017-06-14 10:11:52 -0400

    remove richard.js and migrate listeners to appropriate files

--- app/assets/javascripts/party_show.js
+++ app/assets/javascripts/party_show.js
@@ -191,4 +191,40 @@ $(document).ready(function(){
     })
   })
 
+  $("#drinks-table").on("click", ".item-button", function(e){
+    e.preventDefault();
+    $button=$(this)
+    url = $(this).parent().attr('action')
+    $.ajax({
+      url: url,
+      method: "POST",
+    }).done(function(response){
+      $button.parent().parent().parent().html(response)
+    })
+  })
+
+  $("#dish-table").on("click", ".item-button", function(e){
+    e.preventDefault();
+    $button=$(this)
+    url = $(this).parent().attr('action')
+    $.ajax({
+      url: url,
+      method: "POST",
+    }).done(function(response){
+      $button.parent().parent().parent().html(response)
+    })
+  })
+
+  $("#supply-table").on("click", ".item-button", function(e){
+    e.preventDefault();
+    $button=$(this)
+    url = $(this).parent().attr('action')
+    $.ajax({
+      url: url,
+      method: "POST",
+    }).done(function(response){
+      $button.parent().parent().parent().html(response)
+    })
+  })
+
 })
